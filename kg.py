c3165931ee455d591c5ea5ab82bcf14f746e8608
Sandip Debnath <130656764+Sandip-Debnath@users.noreply.github.com>
2024-05-07 21:33:05 +0530

    Update kg.py

--- kg.py
+++ kg.py
@@ -10,18 +10,10 @@ nlp = spacy.load('en_core_web_sm')
 
 def extract_relationships(sentence):
     relationships = []
-    doc = nlp(sentence)
-    for token in doc:
-        if token.dep_ == 'ROOT':  # Check if token is the root of the sentence
-            verb = token.lemma_
             subject = None
             obj = None  # Initialize object to None
             for child in token.children:
-                if child.dep_ == 'nsubj':  # Check if the child is a subject
-                    subject = child.text
-                elif child.dep_ == 'dobj':  # Check if the child is a direct object
-                    obj = child.text
-            if subject is not None and obj is not None:  # Check if subject and obj are not None
+                if subject is not None and obj is not None:  # Check if subject and obj are not None
                 relationships.append((verb, subject, obj))
     return relationships
 
@@ -31,7 +23,7 @@ csv_files = ['burning.csv', 'antimortem.csv', 'throtlling.csv', 'suicidalpoisoni
 # Iterate over each CSV file
 for csv_file in csv_files:
     # Read the CSV file containing paragraphs
-    df = pd.read_csv(csv_file, encoding='latin1')
+    df = pd.read_csv()
 
     # Create a directed graph
     G = nx.DiGraph()
@@ -41,24 +33,10 @@ for csv_file in csv_files:
         # Split the paragraph into sentences
         sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', paragraph)
 
-        # Extract relationships from each sentence
-        for sentence in sentences:
-            extracted_relationships = extract_relationships(sentence)
-            for verb, subject, obj in extracted_relationships:
-                G.add_edge(subject, obj, label=verb)
-
     # Draw the graph with customized appearance
     plt.figure(figsize=(15, 10))
     pos = nx.spring_layout(G, seed=42, k=2.5)  # Adjust k value for sparser layout
-    nx.draw(G, pos,
-            with_labels=True,
-            node_size=3000,
-            node_color='skyblue',
-            font_size=12,
-            font_weight='bold',
-            arrows=True,
-            arrowsize=20,
-            node_shape='o')
+    nx.draw(G, pos,node_shape='o')
     edge_labels = nx.get_edge_attributes(G, 'label')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
     plt.title(f'Knowledge Graph: {csv_file}')
